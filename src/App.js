47a39d3ec1be1526d551789654caac2fb20aa09a
Roberto <robertocarlosmedina.dev@gmail.com>
2022-05-09 22:53:24 -0100

    Frontend App implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FaExchangeAlt } from "react-icons/fa";
+
+import Api from "./api"
+
+import "./App.css";
 
 function App() {
+  const [languageOption, setLanguageOption] = useState("Cape Verdian Creole");
+  const [translation, setTranslation] = useState(null);
+  const [sourceSenctence, setSourceSencente] = useState("");
+
+  const languageOptionHandler = () => {
+    languageOption === "Cape Verdian Creole"
+      ? setLanguageOption("English")
+      : setLanguageOption("Cape Verdian Creole");
+  };
+
+  const sourceSenctenceHandler = (event) => {
+    setSourceSencente(event.target.value);
+    if(languageOption !== "")
+      getTranslation()
+    else
+      setTranslation(null)
+  };
+
+  const getTranslation = async () => {
+    let target_translation;
+    try {
+      Api.post("/translate/cv/en", {
+        sentence: sourceSenctence
+      }).then((res) => {
+        target_translation = res.data.data[0].translation
+        console.log(target_translation)
+        if(target_translation)
+          setTranslation(target_translation)
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='header-title'>Cape Verdean Creole Translator</h1>
+      <div className='translation-conteiner'>
+        <ul className='languagues-boxes'>
+          <li className='language-display'>
+            <div>
+              <p className='language-selector'>{languageOption}</p>
+              <p>
+                <textarea
+                  className='input-box'
+                  placeholder='Enter Text'
+                  onChange={sourceSenctenceHandler}
+                />
+              </p>
+            </div>
+          </li>
+          <li className='language-display controller-display'>
+            <div>
+              <FaExchangeAlt
+                className='arrows-ajust'
+                onClick={languageOptionHandler}
+              />
+            </div>
+          </li>
+          <li className='language-display'>
+            <div>
+              <p className='language-selector'>
+                {languageOption === "Cape Verdian Creole"
+                  ? "English"
+                  : "Cape Verdian Creole"}
+              </p>
+              <p className={`output-box ${translation ? "with-translation" : "no-translation"}`}>{ translation ? translation : "Translation"}</p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
